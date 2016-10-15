2ab7e5e7c1a01de603bf47f20339d4e9fa24d6dd
John B Garcia <johngarcia9110@gmail.com>
2016-10-15 16:04:39 -0400

    created pages and contact form functionality

--- learning-nodejs/express-website/app.js
+++ learning-nodejs/express-website/app.js
@@ -1,15 +1,59 @@
 var express = require('express');
-var pathmodule = require('path');
+var path = require('path');
 var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
 
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+//middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
+//routes
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.render('index', {title : 'Welcome'});
+});
+
+app.get('/about', function(req, res){
+  res.render('about');
+});
+
+app.get('/contact', function(req, res){
+  res.render('contact');
+});
+
+//contact form data sumbit
+app.post('/contact/send', function(req, res){
+  var transporter = nodemailer.createTransport({
+    service: 'Mailgun',
+    auth: {
+      user: 'postmaster@sandbox63936f25e2344a8c8ba44edddbd6b57d.mailgun.org',
+      pass: '7a2a9f4f5c3cfee6f0cd29bb5d170ad8'
+    }
+  });
+  var mailOptions = {
+    from: 'Test Website <postmaster@sandbox63936f25e2344a8c8ba44edddbd6b57d.mailgun.org>',
+    to: 'johngarcia9110@gmail.com',
+    subject: 'Website Submission',
+    text: 'You have a submission with the following details... Name: ' + req.body.name + ' Email: ' + req.body.email + ' Message: ' + req.body.message,
+    html: '<p> You have a submission with the following details... </p><ul><li>Name:' + req.body.name + '</li><li>Email: ' + req.body.email + '</li><li> Message: ' + req.body.message + '</li><ul>'
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      console.log('error' + error);
+      res.redirect('/');
+    }else{
+      console.log('sent' + info.response);
+      res.redirect('/')
+    }
+  });
+
+
 });
 
 app.listen(3000);
